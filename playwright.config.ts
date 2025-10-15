d454732dec6eaee5a7fdb3f58bca84719718cf35
John Shand <home@Johns-MacBook-Pro-2.local>
2025-10-15 23:33:14 +0100

    feat: bump the workers for faster locally run

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
+  workers: 12,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
